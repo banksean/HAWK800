cdcd7391e3334a404b98ef838fa2b7819d414067
Sean McCullough <banksean@gmail.com>
2021-09-24 20:11:51 -0700

    ¯\_(ツ)_/¯

--- src/go/gen.go
+++ src/go/gen.go
@@ -21,6 +21,7 @@ func main() {
 	}
 
 	// First row is column titles.
+	// Name, CC#, Min, Max, Range
 	for _, row := range rows[1:] {
 		name := strings.Trim(strings.ToTitle(row[0]), " ")
 		for _, c := range ".()/-*" {
@@ -32,5 +33,20 @@ func main() {
 		}
 		fmt.Printf("byte %s = %s;\n", name, row[1])
 	}
-	//fmt.Printf("%+v\n", rows)
+
+	// Array of name strings, indexed by CC#
+	fmt.Printf("const char* const PARAM_NAMES[] =\n{\n")
+	for i, row := range rows[1:] {
+		name := strings.Trim(strings.ToTitle(row[0]), " ")
+		//for _, c := range ".()/-*" {
+		//	name = strings.Replace(name, string(c), "", -1)
+		//}
+		//name = strings.ReplaceAll(name, " ", "_")
+		fmt.Printf("\t\"%s\"", name)
+		if i < len(rows)-2 {
+			fmt.Printf(",")
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("};\n")
 }
