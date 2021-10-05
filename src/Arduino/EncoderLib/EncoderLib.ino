b6ea08c701094386f34695298fe1eb00752f94d5
Sean McCullough <banksean@gmail.com>
2021-10-04 22:12:18 -0700

    ¯\_(ツ)_/¯

--- src/Arduino/EncoderLib/EncoderLib.ino
+++ src/Arduino/EncoderLib/EncoderLib.ino
@@ -3,9 +3,11 @@
 
 #include <MIDI.h>
 //#include <SoftwareSerial.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+//#include <Wire.h>
+//#include <Adafruit_GFX.h>
+#include <U8g2lib.h>
+//#include <Adafruit_SSD1306.h>
+//#include <Adafruit_SH110X.h>
 
 // Random note: https://www.midimonitor.com/ is super helpful in debugging some of this stuff.
 
@@ -20,11 +22,14 @@
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+//#define SCREEN_WIDTH 132 // OLED display width, in pixels
+//#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+//Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+//U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, A5, A4);
+
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(U8G2_R0, A5, A4);
 
 bool update = false;
 
@@ -69,20 +74,40 @@ void setup() {
           memcpy_P(&param, &(ALL_PARAMS[i]), sizeof(Param));
     PARAM_VALUES[i] = param.min;
   }
-
- if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_5x7_f);
+  u8x8.drawString(0, 0, "ok");
+  
+  /*
+  u8g2.initDisplay();
+  u8g2.begin();
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g_font_5x8);
+  u8g2.setCursor(0, 8);
+  u8g2.print("ok");
+  u8g2.setCursor(0, 16);
+  u8g2.print("ok");
+  u8g2.setCursor(0, 24);
+  u8g2.print("ok");
+  u8g2.sendBuffer();
+  */
+/*
+ if(!display.begin(0x3C, true)) { //SH110X_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     //Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
+  
   delay(2000);
   display.clearDisplay();
 
   display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.cp437(true); // Use full 256 char 'Code Page 437' font
+  display.setTextColor(SH110X_WHITE);
+  //display.cp437(true); // Use full 256 char 'Code Page 437' font
   display.setCursor(0, 10);
   display.print(F("ok"));
   display.display();
+  (
+  */
 }
 
 
@@ -104,10 +129,10 @@ void handleNoteOn(byte channel, byte pitch, byte velocity) {
   }
 
   MIDI.sendNoteOn(pitch, velocity, channel);
-  display.setCursor(0, 40);
+  //display.setCursor(0, 40);
   // Display static text
-  display.fillRect(0, 40, SCREEN_WIDTH, 10, BLACK);
-  display.println("Note ON");
+  //display.fillRect(0, 40, SCREEN_WIDTH, 10, SH110X_BLACK);
+  //display.println("Note ON");
   update = true;
   //display.display(); 
 }
@@ -119,10 +144,10 @@ void handleNoteOff(byte channel, byte pitch, byte velocity) {
   
   MIDI.sendNoteOff(pitch, velocity, channel);
   MIDI.sendNoteOn(pitch, velocity, channel);
-  display.setCursor(0, 40);
+  //display.setCursor(0, 40);
   // Display static text
-  display.fillRect(0, 40, SCREEN_WIDTH, 10, BLACK);
-  display.println("Note OFF");
+  //display.fillRect(0, 40, SCREEN_WIDTH, 10, SH110X_BLACK);
+  //display.println("Note OFF");
   update = true;
 }
 
@@ -130,12 +155,15 @@ void handleNoteOff(byte channel, byte pitch, byte velocity) {
 void loop() {
   update = false;
   static char buff[16];
+  bool updateRow[NUM_ENCODERS];
+  
   for (int i = 0; i < NUM_ENCODERS; i = i + 1) {
 
     // getButtonPressed is really "button down" state. So it's true as long as the encoder is
     // held down.
+    updateRow[i] = false;
     if (ENCODERS[i]->checkButtonUp() == 1) {
-        update = true;
+        updateRow[i] = true;
         if (ENCODERS[i]->getMode() == 1) {
           // just entered into param select mode
           // don't really care what getValue() returns at this point.
@@ -154,7 +182,7 @@ void loop() {
     }
 
     if( ENCODERS[i]->updateRotary() ) {
-      update = true;
+      updateRow[i] = true;
       int v = ENCODERS[i]->getValue();
       if (ENCODERS[i]->getMode() == 1) {
         SELECTED_PARAM[i] = v;
@@ -166,30 +194,46 @@ void loop() {
         //MIDI.sendControlChange(84, param.cc, MIDI_CH);
         //MIDI.sendControlChange(85, v, MIDI_CH);
       }
-    }
-
-    // Update the display.
-    if (update) {
-        int v = ENCODERS[i]->getValue();
-      display.setCursor(0, 10 * (i + 1));
-      display.fillRect(0, 10 * (i + 1), SCREEN_WIDTH, 10, BLACK);
-      int param = SELECTED_PARAM[i];
-      char * ptr = (char *) pgm_read_word (&PARAM_NAMES [param]);
-      // TODO: just copy this straight into buff, instead of intermediate paramName?
-      char paramName [16]; // must be large enough!
-      strcpy_P (paramName, ptr);
-
-      // Render string based on encoder's mode.
-      if (ENCODERS[i]->getMode() == 1) {
-        sprintf(buff, "%d: [%s]", v, paramName);
-      } else {
-        sprintf(buff, "%s: %d", paramName, v);
-      }
-      display.println(buff);
-    }
-    
+    } 
+    update = update || updateRow[i];
   }
+
   if (update) {
-    display.display(); 
+    //u8g2.firstPage();
+  //u8g2.nextPage();
+    //do {
+  for (int i = 0; i < NUM_ENCODERS; ++i) {
+        // Update the display.
+    if (updateRow[i]) {
+      u8x8.clearLine(i);
+      //u8g2.clearBuffer();
+          int v = ENCODERS[i]->getValue();
+          int param = SELECTED_PARAM[i];
+          char * ptr = (char *) pgm_read_word (&PARAM_NAMES [param]);
+          // TODO: just copy this straight into buff, instead of intermediate paramName?
+          char paramName [16]; // must be large enough!
+          strcpy_P (paramName, ptr);
+  
+          // Render string based on encoder's mode.
+          if (ENCODERS[i]->getMode() == 1) {
+            u8x8.setInverseFont(1);
+            sprintf(buff, "%s: %d", paramName, PARAM_VALUES[v]);
+          } else {
+            u8x8.setInverseFont(0);
+            sprintf(buff, "%s: %d", paramName, v);
+          }
+          u8x8.drawString(0, i, buff);
+          //u8g2.setDrawColor(0);
+          //u8g2.drawBox(0, 8*i, 128, 8);
+          //u8g2.setDrawColor(1);
+          //u8g2.setCursor(0, 8*(i+1));
+          //u8g2.print(buff);
+
+          //u8g2.updateDisplay();
+          //u8g2.updateDisplayArea(0, i, 128/8, 1);
+          //u8g2.drawStr(0, 8 * (i + 1), buff);
+    }
+    } 
+    //} while (u8g2.nextPage());
   }
 }
